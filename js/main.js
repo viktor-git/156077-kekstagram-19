043f43b6f65c2cdbec90d919ecd0a3799329a09f
viktor-git <52155112+viktor-git@users.noreply.github.com>
2020-01-31 18:12:52 +0300

    Добавлена функция рандома

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@
   var photos = [];
   var photo = {};
 
-  var randomInfo = function () {
-
-  }
-
+  var likeMin = 15;
+  var likeMax = 200;
 
   var commentsText = ['Всё отлично!',
                   'В целом всё неплохо. Но не всё.',
@@ -14,35 +12,33 @@
                   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
                   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
   ];
-  var randomMessage = commentsText[Math.round(Math.random() * (commentsText.length - 1))];
-
   var names = ['Евлампия', 'Alalay-balalay', 'Акакий', 'Вася Имбицил'];
+  var comments = [];
+  var randomMessage = '';
+  var randomAvatar = 0;
 
-  var randomAvatar = Math.round(Math.random() * (6 - 1) + 1);
-
-  var comment = [
-    {
+  var randomInfo = function () {
+    randomMessage = commentsText[Math.round(Math.random() * (commentsText.length - 1))];
+    randomAvatar = Math.round(Math.random() * (6 - 1) + 1);
+    var comment = {
       avatar: `../img/avatar-${randomAvatar}.svg`,
       message: randomMessage,
       name: names[Math.round(Math.random() * (names.length - 1))]
-    }
-  ];
+    };
+    comments.push(comment);
+  }
 
-  var likeMin = 15;
-  var likeMax = 200;
-
-var createPhotoDescr = function (copies) {
+  var createPhotoDescr = function (copies) {
   for (var i = 1; i <= copies; i++) {
     randomInfo();
-
-    photo['url'] = '`photos/${i}.jpg`';
+    photo['url'] = `photos/${i}.jpg`;
     photo['description'] = 'Описание моей фотографии такое классное';
     photo['likes'] = Math.round(Math.random() * (likeMax - likeMin) + likeMin);
-    photo['comments'] = comment[0];
-    console.log(photo);
+    photo['comments'] = comments[Math.round(Math.random() * (comments.length - 1))];
+    photos.push(photo);
+     console.log(photos);
   }
 }
-
-createPhotoDescr(10);
+createPhotoDescr(4);
 
 
