9f94a8aead9dc6505a0bd55d7d3303f21c88a9f5
viktor-git <52155112+viktor-git@users.noreply.github.com>
2020-02-03 17:58:24 +0300

    Добавлен вывод фото в DOM

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 'use strict'
   var photos = [];
-  var photo = {};
+  var comments = [];
+
   var likeMin = 15;
   var likeMax = 200;
+
   var avatarNumberMin = 1;
   var avatarNumberMax = 6;
+
   var commentsText = ['Всё отлично!',
                   'В целом всё неплохо. Но не всё.',
                   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -13,33 +16,74 @@
                   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
   ];
   var names = ['Евлампия', 'Alalay-balalay', 'Акакий', 'Вася Имбицил'];
-  var comments = [];
 
+//Добавляем метод рандомизации элементов массива
+  Object.defineProperty(
+    Object.prototype,
+    'randElement',
+    {
+      value: function() {
+          var rand = Math.floor(Math.random() * this.length);
+          return this[rand];
+        }
+    }
+);
+
+//Функция для создания массива рандомных комментариев к фото
   var getRandomInfo = function () {
-    var randomMessage = commentsText[Math.floor(Math.random() * commentsText.length)];
+    var randomMessage = commentsText.randElement();
     var randomAvatar = Math.floor(Math.random() * (avatarNumberMax - avatarNumberMin) + avatarNumberMin);
     var randomComment = {
-      avatar: `../img/avatar-${randomAvatar}.svg`,
+      avatar: `img/avatar-${randomAvatar}.svg`,
       message: randomMessage,
-      name: names[Math.floor(Math.random() * names.length)]
+      name: names.randElement()
     };
     comments.push(randomComment);
-    /*for (var i = 0; i < comments.length; i++) {
-      console.log(comments[i]);
-    }*/
   }
 
-  var createPhotoDescr = function (copies) {
+//Функция создания пользовательских объектов с фото
+  var addPhotos = function (copies) {
+  var photo = {};
   for (var i = 1; i <= copies; i++) {
+    //debugger;
     getRandomInfo();
     photo['url'] = `photos/${i}.jpg`;
     photo['description'] = 'Описание моей фотографии такое классное';
     photo['likes'] = Math.floor(Math.random() * (likeMax - likeMin) + likeMin);
-    photo['comment'] = comments[Math.floor(Math.random() * comments.length)];
-    photos.push(photo);
-    console.log(photos[i - 1].comment);
+    photo['comment'] = comments.randElement();
+    var clonePhoto = Object.assign({}, photo);
+//Тут у меня какая-то ерунда с добавлением в массив photos. Все внутренние объекты получаются одинаковые,
+//причем в функции выше в массив comments попадают разные объекты без клонирования.
+    photos.push(clonePhoto);
   }
 }
-createPhotoDescr(25);
+
+//Создаем фрагмент с фото для добавления в DOM на основе шаблона
+  var createPictures = function () {
+    addPhotos(25);
+
+    var pictureTemplate = document.querySelector('#picture').content;
+    var fragment = new DocumentFragment();
+
+    for (var i = 0; i < photos.length; i++) {
+      var newPhoto = pictureTemplate.cloneNode(true);
+      newPhoto.querySelector('.picture__img').src = photos[i].url;
+      newPhoto.querySelector('.picture__likes').textContent = photos[i].likes;
+      newPhoto.querySelector('.picture__comments').textContent = 1;
+
+      fragment.append(newPhoto);
+  }
+    return fragment;
+}
+
+//Вставляем созданный фрагмент в DOM
+  var addPicturesToDom = function () {
+    document.querySelector('.pictures').append(createPictures());
+}
+
+addPicturesToDom();
+
+
+
 
 
