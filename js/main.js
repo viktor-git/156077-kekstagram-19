fff1004353255b3597c53bbdbd3bcfb009bd434d
viktor-git <52155112+viktor-git@users.noreply.github.com>
2020-02-07 11:57:22 +0300

    Внесены изменения по замечаниям

--- js/main.js
+++ js/main.js
@@ -19,67 +19,80 @@ var commentsText = ['Всё отлично!',
 
 var names = ['Евлампия', 'Alalay-balalay', 'Акакий', 'Вася Имбицил', 'Дрын Петрович', 'Марина Золотце'];
 
+
+//  Функция для создания объекта с комментарием
+var createComment = function () {
+  var randomAvatar = getRandomNum(1, 6);
+  var randomComment = {
+    avatar: 'img/avatar-' + randomAvatar + '.svg',
+    message: getRandElement(commentsText),
+    name: getRandElement(names)
+  };
+
+  return randomComment;
+};
+
 //  Функция для создания массива рандомных комментариев к фото
 var getRandomComments = function () {
-
   var comments = [];
   var randCommentsQuantity = getRandomNum(1, 6);
 
   for (var i = 0; i <= randCommentsQuantity; i++) {
-    var randomMessage = getRandElement(commentsText);
-    var randomAvatar = getRandomNum(1, 6);
-    var randomComment = {
-      avatar: 'img/avatar-' + randomAvatar + '.svg',
-      message: randomMessage,
-      name: getRandElement(names)
-    };
-    comments.push(randomComment);
+    comments.push(createComment());
   }
 
   return comments;
 };
 
-//  Функция создания пользовательских объектов с фото
-var addPhotos = function (copies) {
-
-  var photos = [];
+//  Функция создания пользовательсого объекта с фото
+var createPhoto = function (num) {
   var photo = {};
 
+  photo['url'] = 'photos/' + num + '.jpg';
+  photo['description'] = 'Описание моей фотографии такое классное';
+  photo['likes'] = getRandomNum(15, 200);
+  photo['comments'] = getRandomComments();
+
+  var clonePhoto = Object.assign({}, photo);
+
+  return clonePhoto;
+};
+
+//  Функция создания пользовательсого массива объектов с фото
+var addPhotos = function (copies) {
+  var photos = [];
+
   for (var i = 1; i <= copies; i++) {
-    var photoComments = getRandomComments();
-    photo['url'] = 'photos/' + i + '.jpg';
-    photo['description'] = 'Описание моей фотографии такое классное';
-    photo['likes'] = getRandomNum(15, 200);
-    photo['comment'] = photoComments;
-    var clonePhoto = Object.assign({}, photo);
-    photos.push(clonePhoto);
+    photos.push(createPhoto(i));
   }
 
   return photos;
 };
 
-//  Создаем фрагмент с фото для добавления в DOM на основе шаблона
-var createPictures = function () {
+// Получаем массив данных о фотографии
+var userPhotos = addPhotos(25);
 
-  var pictureTemplate = document.querySelector('#picture').content;
+//  Создаем фото для добавления в DOM на основе шаблона
+var createPicture = function (num) {
+  var pictureTemplateClone = document.querySelector('#picture').content.cloneNode(true);
+
+  pictureTemplateClone.querySelector('.picture__img').src = userPhotos[num].url;
+  pictureTemplateClone.querySelector('.picture__likes').textContent = userPhotos[num].likes;
+  pictureTemplateClone.querySelector('.picture__comments').textContent = userPhotos[num].comments.length;
+
+  return pictureTemplateClone;
+};
+
+//  Создаем фрагмент с фотографиями на основе шаблона добавления в DOM
+var addPictures = function () {
   var fragment = new DocumentFragment();
 
   for (var i = 0; i < userPhotos.length; i++) {
-    var newPhoto = pictureTemplate.cloneNode(true);
-    newPhoto.querySelector('.picture__img').src = userPhotos[i].url;
-    newPhoto.querySelector('.picture__likes').textContent = userPhotos[i].likes;
-    newPhoto.querySelector('.picture__comments').textContent = userPhotos[i].comment.length;
-
-    fragment.append(newPhoto);
+    fragment.append(createPicture(i));
   }
 
   return fragment;
 };
 
 //  Вставляем созданный фрагмент фотографий в DOM
-var addPicturesToDom = function () {
-  document.querySelector('.pictures').append(createPictures());
-};
-
-var userPhotos = addPhotos(25);
-addPicturesToDom();
+document.querySelector('.pictures').append(addPictures());
