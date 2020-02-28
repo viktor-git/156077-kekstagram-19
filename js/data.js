30831bfbd99df2f38582c3a831da94b97b1b65ab
viktor-git <52155112+viktor-git@users.noreply.github.com>
2020-02-28 12:43:06 +0300

    Доработаны ошибки сервера

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@
 (function () {
 
   var URL = 'https://js.dump.academy/kekstagram/data';
-  var TIMEOUT_IN_MS = 10000;
+  var TIMEOUT_IN_MS = 100000;
 
   window.load = function (onSuccess, dataId) {
     var xhr = new XMLHttpRequest();
@@ -13,26 +13,37 @@
       if (xhr.status === 200) {
         onSuccess(xhr.response, dataId);
       } else {
-        showError('Статус ответа: ' + xhr.status);
+        showError('Статус ответа: ' + xhr.status, 'Проверьте корректность запрашиваемого URL');
       }
     });
 
-    xhr.open('GET', URL);
-    xhr.send();
+    var showError = function (errorMessage, addMessage) {
+      var errorMess = document.createElement('div');
+      errorMess.textContent = errorMessage;
+      errorMess.classList.add('server-error')
 
-    var showError = function (errorMessage) {
-      console.log(errorMessage);
+      var additionalMessage = document.createElement('p');
+      additionalMessage.classList.add('server-error__descr');
+      additionalMessage.textContent = addMessage;
+
+      errorMess.append(additionalMessage);
+      document.body.prepend(errorMess);
+
     };
 
     xhr.addEventListener('error', function () {
-      showError('Произошла ошибка соединения');
+      showError('Произошла ошибка соединения', 'Вы оффлайн. Проверьте подключение к интернету');
     });
     xhr.addEventListener('timeout', function () {
-      showError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      showError('Запрос не успел выполниться за ' + xhr.timeout + 'мс', 'Проверьте скорость подключения');
     });
 
     xhr.timeout = TIMEOUT_IN_MS;
 
+    xhr.open('GET', URL);
+    xhr.send();
   };
+
+
 
 })();
