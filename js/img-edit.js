bb29b6f2098e927702a1538b2a89d4283aa66ad2
viktor-git <52155112+viktor-git@users.noreply.github.com>
2020-02-28 18:55:53 +0300

    Исправлено событие при закрытии

--- js/img-edit.js
+++ js/img-edit.js
@@ -235,28 +235,28 @@
       document.addEventListener('keydown', errorMessageRemoveKeyHandler);
     };
 
-    var successMessageRemoveClickHandler = function () {
-      if (evt.target.classList.contains('success__button') ||
-        evt.target.classList.contains('success')) {
+    var successMessageRemoveClickHandler = function (evtClick) {
+      if (evtClick.target.classList.contains('success__button') ||
+        evtClick.target.classList.contains('success')) {
         closeMessage('main .success');
       }
     };
 
-    var successMessageRemoveKeyHandler = function () {
+    var successMessageRemoveKeyHandler = function (evtClick) {
       if (evt.key === 'Escape') {
         closeMessage('main .success');
       }
     };
 
-    var errorMessageRemoveClickHandler = function () {
-      if (evt.target.classList.contains('error__button') ||
-        evt.target.classList.contains('error')) {
+    var errorMessageRemoveClickHandler = function (evtKey) {
+      if (evtKey.target.classList.contains('error__button') ||
+        evtKey.target.classList.contains('error')) {
         closeMessage('main .error');
       }
     };
 
-    var errorMessageRemoveKeyHandler = function () {
-      if (evt.key === 'Escape') {
+    var errorMessageRemoveKeyHandler = function (evtKey) {
+      if (evtKey.key === 'Escape') {
         closeMessage('main .error');
       }
     };
