e989faa86a9785c28d75c9c661868a7cc61a921b
viktor-git <52155112+viktor-git@users.noreply.github.com>
2020-03-02 14:28:34 +0300

    Трэвис

--- js/data.js
+++ js/data.js
@@ -1,12 +1,10 @@
 'use strict';
 (function () {
 
-  //var URL = 'https://js.dump.academy/kekstagram/data';
   var TIMEOUT_IN_MS = 10000;
 
   window.load = function (type, url, onSuccess, onError, data, DataId) {
     var xhr = new XMLHttpRequest();
-    console.log(xhr);
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
@@ -14,21 +12,21 @@
         onSuccess(xhr.response, DataId);
       } else {
         (type === 'GET') ?
-        onError('Статус ответа: ' + xhr.status, 'Проверьте корректность запрашиваемого URL') :
-        onError();
+          onError('Статус ответа: ' + xhr.status, 'Проверьте корректность запрашиваемого URL') :
+          onError();
       }
     });
 
     xhr.addEventListener('error', function () {
       (type === 'GET') ?
-      onError('Произошла ошибка соединения', 'Вы оффлайн. Проверьте подключение к интернету') :
-      onError(xhr.status);
+        onError('Произошла ошибка соединения', 'Вы оффлайн. Проверьте подключение к интернету') :
+        onError(xhr.status);
     });
 
     xhr.addEventListener('timeout', function () {
       (type === 'GET') ?
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс', 'Проверьте скорость подключения') :
-      onError(TIMEOUT_IN_MS);
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс', 'Проверьте скорость подключения') :
+        onError(TIMEOUT_IN_MS);
     });
 
     xhr.timeout = TIMEOUT_IN_MS;
